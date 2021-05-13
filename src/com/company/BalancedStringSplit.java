749ee36b19f7154d5294192ca9d13aa81a76ce06
ted404 <tyahyayev@gmail.com>
2021-05-14 01:28:21 +0500

    balancedStringSplit

--- src/com/company/BalancedStringSplit.java
+++ src/com/company/BalancedStringSplit.java
@@ -1,2 +1,35 @@
-package com.company;public class BalancedStringSplit {
+package com.company;
+
+public class BalancedStringSplit {
+    //Runtime: 100%
+    //Memory: 47%
+    //If chartAt(x) equals to 'L' (or 'R' doesn't really matter) count +1.
+    //Else count -1.
+    //Checking in each iteration to count = 0; if true result is getting +1.
+    //Loops till the end of s.length()-1;
+
+    public int balancedStringSplit(String s) {
+
+        StringBuilder e = new StringBuilder(s);
+        int result = 0;
+        int count = 0;
+        int i = 0;
+
+        while(i <= e.length()-1){
+
+            if(e.charAt(i) == 'L'){
+                count++;
+                i++;
+            }else{
+                count--;
+                i++;
+            }
+
+            if(count == 0){
+                result++;
+            }
+        }
+        return result;
+    }
+
 }
