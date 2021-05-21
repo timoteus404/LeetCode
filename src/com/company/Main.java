264a9fad359bbb1fbd55b1c0ceeed069453f8d29
timoteus404 <yakhyoev.temur@mail.ru>
2021-05-21 16:16:46 +0500

    merge sort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+            int[] array = {9,2,1,11,8,0,18};
 
+            var a = MergeSort.mergeSort(array);
+
+            for(int i: a){
+                System.out.print(i + " ");
+            }
 
     }
 }
---
+++ src/com/company/MergeSort.java
@@ -0,0 +1,58 @@
+package com.company;
+
+public class MergeSort {
+
+    public static int[] mergeSort(int[] array){
+        if(array.length <= 1){
+            return array;
+        }
+        int midpoint = array.length/2;
+        int[] left = new int[midpoint];
+        int[] right;
+        if(array.length%2==0){
+            right = new int[midpoint];
+        }else {
+            right = new int[midpoint+1];
+        }
+
+        for(int i=0; i<midpoint; i++){
+            left[i] = array[i];
+        }
+
+        for(int j = 0; j<right.length; j++){
+            right[j] = array[midpoint+j];
+        }
+
+        int[] result = new int[array.length];
+
+        left = mergeSort(left);
+        right = mergeSort(right);
+
+        result = merge(left, right);
+
+        return result;
+    }
+
+    private static int[] merge(int[] left, int[] right){
+        int[] result = new int[left.length + right.length];
+
+        int leftPointer, rightPointer, resultPointer;
+        leftPointer = rightPointer = resultPointer = 0;
+
+        while(leftPointer < left.length || rightPointer < right.length){
+
+            if(leftPointer < left.length && rightPointer < right.length){
+                if(left[leftPointer]<right[rightPointer]){
+                    result[resultPointer++] = left[leftPointer++];
+                }else{
+                    result[resultPointer++] = right[rightPointer++];
+                }
+            }else if(leftPointer <left.length){
+                result[resultPointer++] = left[leftPointer++];
+            }else if(rightPointer<right.length){
+                result[resultPointer++] = right[rightPointer++];
+            }
+        }
+        return result;
+    }
+}
